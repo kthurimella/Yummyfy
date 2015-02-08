3b70879996462ad14d76c59db647167ba6574900
Kumar Thurimella <Kumar.Thurimella@colorado.edu>
2015-02-07 17:20:31 -0800

    Yummify - [Kumar] - configured db connection

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 MONGODB_SETTINGS = {'DB': 'heroku_app33806434',
-                    'host': 'mongodb://heroku_app33806434@ds041821.mongolab.com:41821/heroku_app33806434'
+                    'host': 'mongodb://heroku_app33806434:st581cog5a31e5n2s2bg0i173u@ds041651.mongolab.com:41651/heroku_app33806434'
 }
