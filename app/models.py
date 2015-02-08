4a53a1073b519c23d47014842e9ed2275dc7d9b1
Vijay Thurimella <vthurimella@cmu.edu>
2015-02-07 16:10:00 -0800

    login page

--- app/models.py
+++ app/models.py
@@ -6,5 +6,3 @@ class User(db.Document):
   created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
   email = db.StringField(max_length=255, required=True)
   password = db.StringField(max_length=255, required=True)
-
-
