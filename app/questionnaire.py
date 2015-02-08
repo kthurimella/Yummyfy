281afbe8b841269b3f07ab3f8b834fc2bd294ecb
Kumar Thurimella <Kumar.Thurimella@colorado.edu>
2015-02-08 11:16:09 -0800

    Yummify - [Kumar] - added first assessment from traitify using sandbox

--- app/questionnaire.py
+++ app/questionnaire.py
@@ -2,10 +2,16 @@ __author__ = 'kthurime'
 
 from libraries.traitify import Traitify
 
-secrect_key = "13ba33b4bi33jlq1lnu9i3oroe"
+class Questionnaire:
 
-def create_new_questionnaire():
-    traitify_instance = Traitify(secrect_key)
-    assessment = traitify_instance.create_assessment("core")
+    def __init__(self, deck_id):
+        self.secrect_key = "13ba33b4bi33jlq1lnu9i3oroe"
+        self.traitify_instance = Traitify(self.secrect_key)
+        self.assessment = self.traitify_instance.create_assessment(deck_id)
 
+    def create_new_questionnaire(self):
+        return self.traitify_instance.get_assessment(self.assessment.id)
 
+    def get_assessment_results(self):
+        personality_types = self.traitify_instance.get_personality_types(self.assessment.id)
+        return personality_types["personality_types"][0].personality_type
